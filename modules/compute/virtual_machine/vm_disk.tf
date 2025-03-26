863dfc24d53c6d5ca29e40c24b0f94ebda6b75db
SoorvirSinghNegi <159767206+SoorvirSinghNegi@users.noreply.github.com>
2025-03-26 09:21:02 +0000

    Update vm_disk.tf adding the attribute source_resource_id as it is required to create a data disk with an existing data disk (#29)

--- modules/compute/virtual_machine/vm_disk.tf
+++ modules/compute/virtual_machine/vm_disk.tf
@@ -24,7 +24,7 @@ resource "azurerm_managed_disk" "disk" {
   disk_mbps_read_write   = try(each.value.disk_mbps_read_write, null)
   tags                   = merge(local.tags, try(each.value.tags, {}))
   disk_encryption_set_id = try(each.value.disk_encryption_set_key, null) == null ? null : var.disk_encryption_sets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.disk_encryption_set_key].id
-
+  source_resource_id     = try(each.value.source_resource_id, null) # using this we will be able to create the data disk by referencing a data disk.
   lifecycle {
     ignore_changes = [
       name, #for ASR disk restores
