ab0bf611fdeca5599ff3bb99713fcf57ebc6f194
sagar-dholakia <sagar.dholakia@lv.com>
2025-04-11 10:52:23 +0000

    ignore for auth_key erc

--- modules/networking/virtual_network_gateway_connections/module.tf
+++ modules/networking/virtual_network_gateway_connections/module.tf
@@ -52,7 +52,7 @@ resource "azurerm_virtual_network_gateway_connection" "vngw_connection" {
   }
 
   lifecycle {
-    ignore_changes = [virtual_network_gateway_id]
+    ignore_changes = [virtual_network_gateway_id, authorization_key]
   }
 
 }
