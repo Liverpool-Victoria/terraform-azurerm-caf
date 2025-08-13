d3865511b16fea92c60436d0405ba56b2b45239b
sagar-dholakia <67733963+sagar-dholakia@users.noreply.github.com>
2025-08-13 13:50:52 +0100

    Update server.tf
    add auto grow for flex servers

--- modules/databases/postgresql_flexible_server/server.tf
+++ modules/databases/postgresql_flexible_server/server.tf
@@ -20,6 +20,8 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
   delegated_subnet_id = var.remote_objects.subnet_id
   private_dns_zone_id = var.remote_objects.private_dns_zone_id
 
+  auto_grow_enabled = try(var.settings.auto_grow_enabled, false)
+
   create_mode                       = try(var.settings.create_mode, "Default")
   point_in_time_restore_time_in_utc = try(var.settings.create_mode, "PointInTimeRestore") == "PointInTimeRestore" ? try(var.settings.point_in_time_restore_time_in_utc, null) : null
   source_server_id                  = try(var.settings.create_mode, "PointInTimeRestore") == "PointInTimeRestore" ? try(var.settings.source_server_id, null) : null
