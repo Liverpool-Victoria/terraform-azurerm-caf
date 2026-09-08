120acd9da8f7d5a008c9da5e6c25ad91409c4296
SoorvirSinghNegi <159767206+SoorvirSinghNegi@users.noreply.github.com>
2026-09-08 09:02:54 +0530

    Update networking_vpn_gateway_connection.tf

--- networking_vpn_gateway_connection.tf
+++ networking_vpn_gateway_connection.tf
@@ -14,7 +14,6 @@ module "vpn_gateway_connections" {
   source     = "./modules/networking/vpn_gateway_connection"
   for_each   = local.networking.vpn_gateway_connections
   
-  key_vault_id    = module.key_vault.key_vault_id
   settings        = each.value
   global_settings = local.global_settings
   client_config   = local.client_config
