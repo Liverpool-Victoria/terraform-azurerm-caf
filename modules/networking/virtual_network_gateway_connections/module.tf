32f870f8426caf9e75488c443e43d63986cb8703
SoorvirSinghNegi <159767206+SoorvirSinghNegi@users.noreply.github.com>
2026-09-08 09:23:03 +0530

    Update module.tf

--- modules/networking/virtual_network_gateway_connections/module.tf
+++ modules/networking/virtual_network_gateway_connections/module.tf
@@ -65,9 +65,9 @@ resource "azurerm_virtual_network_gateway_connection" "vngw_connection" {
 #  key_vault_id = var.key_vault_id
 #}
 data "azurerm_key_vault_secret" "shared_key" {
-  name     = var.settings.shared_key_secret_name.secret_name
+  name     = var.shared_key_secret_name.secret_name
   key_vault_id = try(
-    var.settings.shared_key_secret_name.key_vault_id,
-    try(var.keyvaults[settings.shared_key_secret_name.lz_key][var.settings.shared_key_secret_name.keyvault_key].id, var.keyvaults[var.client_config.landingzone_key][var.settings.shared_key_secret_name.keyvault_key].id)
+    var.shared_key_secret_name.key_vault_id,
+    try(var.keyvaults[shared_key_secret_name.lz_key][var.shared_key_secret_name.keyvault_key].id, var.keyvaults[var.client_config.landingzone_key][var.shared_key_secret_name.keyvault_key].id)
   )
 }
